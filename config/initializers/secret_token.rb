594fc13dbc6f098b9ae54aa74baefde5ceb23d39
git-rahulmeena13 <git-rahulmeena@live.com>
2014-05-11 22:36:43 +0530

    Initializing Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zomato::Application.config.secret_key_base = 'ae19d31b337810a38f1b9584842383c5dbf2ad20bf1a640fb5e588f2935098169f1994c98f4fd39ef328789bc8ac006bad0c4ddb246c9bca3d7cacaaacc224f7'
