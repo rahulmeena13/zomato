798e496559c1a5ac6220d74a9e098b5e04667552
git-rahulmeena13 <git-rahulmeena@live.com>
2014-05-13 01:41:13 +0530

    changes Readme

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,7 +2,7 @@ class Rating < ActiveRecord::Base
   belongs_to :restaurant, :counter_cache => true
   belongs_to :review
   validates :restaurant_id, presence: true
-  validates :value, presence: true
+  validates :value, presence: true, :numericality => { :greater_than_or_equal_to => 10, :less_than_or_equal_to => 50 }
   after_create :recalculate_average
 
   private
